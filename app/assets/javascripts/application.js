210cb13e5704c51de844c0fc63276b134262aea1
Erin Mahon <erin.mahon14@gmail.com>
2016-02-05 14:46:52 -0500

    initial layout for image slider on homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,25 @@
 //= require turbolinks
 //= require_tree .
 
+// SLIDER
+var currentIndex = 0,
+  items = $('.container div'),
+  itemAmt = items.length;
+
+function cycleItems() {
+  var item = $('.container div').eq(currentIndex);
+  items.hide();
+  item.css('display','inline-block');
+}
+
+var autoSlide = setInterval(function() {
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+}, 2000);
+
 
 // hamburger menu
 $(document).ready(function() {
@@ -27,3 +46,9 @@ $(document).ready(function() {
 
 
 });
+
+
+
+
+
+
